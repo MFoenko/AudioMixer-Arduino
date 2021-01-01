f0f954f37744bdb74fdd2476d627504ab2bd1927
mfoenko <mfoenko@gmail.com>
2021-01-01 16:22:34 -0500

    added commands to fetch number of controls and value of a particular control

--- volume.ino
+++ volume.ino
@@ -1,5 +1,7 @@
-const int LEN = 1;
-int count = 0;
+const int LEN = 2;
+
+const int COMMAND_GET_SIZE = 'C';
+const int COMMAND_GET_VALUE = 'G';
 
 struct Control{
   int pinout;
@@ -13,13 +15,25 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   controls[0].pinout = A0;
+  controls[1].pinout = A1;
 }
 
 bool on = false;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.read();
+  int command = Serial.read();
+
+  switch(command){
+    case COMMAND_GET_SIZE:
+      Serial.write(LEN);
+      break;
+    case COMMAND_GET_VALUE:
+      int index = Serial.read();
+      Serial.write(controls[index].val);
+      break;
+  }
+  
   for(int i=0;i<LEN;i++){
     Control* c = &controls[i];
     c->val = analogRead(c->pinout)/4;
@@ -29,6 +43,5 @@ void loop() {
       c->oldVal = c->val;
     }
   }
-  count++;
   delay(100);
 }
